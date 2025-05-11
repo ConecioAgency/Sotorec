3c75afbc1330c78b6cb04faa593d2ff85caf9633
ConecioAgency <conecioagency@gmail.com>
2025-05-11 22:41:59 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <Navbar />
       
       {/* Hero Section */}
-      <section className="relative h-screen flex items-center justify-center bg-black text-white overflow-hidden">
+      <section className="relative h-screen flex items-center justify-center bg-black text-white">
         {/* Vidéo de fond */}
         <video
           autoPlay
